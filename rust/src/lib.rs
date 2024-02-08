c04fd4fb4da92c8f865eea3273ab6dee82e0115e
benni347 <cdrc@skwar.me>
2024-02-08 20:21:24 +0100

    tests

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -21,3 +21,32 @@ pub fn sieve(limit: usize) -> Vec<usize> {
         .filter_map(|(i, prime)| if prime { Some(i) } else { None })
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_zero_and_one() {
+        assert_eq!(sieve(0), vec![]);
+        assert_eq!(sieve(1), vec![]);
+    }
+
+    #[test]
+    fn test_small_limit() {
+        assert_eq!(sieve(10), vec![2, 3, 5, 7]);
+    }
+
+    #[test]
+    fn test_larger_limit() {
+        assert_eq!(sieve(30), vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+    }
+
+    #[test]
+    fn test_edge_cases() {
+        assert_eq!(sieve(2), vec![2]);
+        assert_eq!(sieve(3), vec![2, 3]);
+        assert_eq!(sieve(4), vec![2, 3]);
+        assert_eq!(sieve(5), vec![2, 3, 5]);
+    }
+}
